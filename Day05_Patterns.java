292acab9e6f786c0c514d5761bb4860b7fc43bb6
vasistacv <vasisthamanju796@gmail.com>
2026-02-09 20:25:43 +0530

    Day05 Star Pattern

---
+++ Day05_Patterns.java
@@ -0,0 +1,26 @@
+
+/**
+ * Day 05: Star Pattern
+ * Printing rectangle of stars using nested loops.
+ */
+
+import java.util.*;
+
+public class Day05_Patterns {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Enter the number of rows:");
+        int n = sc.nextInt();
+        System.out.println("Enter the number of columns:");
+        int m = sc.nextInt();
+
+        for (int i = 1; i <= n; i++) {
+            for (int j = 1; j <= m; j++) {
+                System.out.print("*");
+            }
+            System.out.println("*");
+        }
+
+    }
+
+}
---
+++ Day05_Patterns.py
@@ -0,0 +1,12 @@
+"""
+Day 05: Star Pattern
+Printing rectangle of stars using nested loops.
+"""
+
+n = int(input("Enter the number of rows:"))
+m = int(input("Enter the number of columns:"))
+
+for i in range(1, n + 1):
+    for j in range(1, m + 1):
+        print("*", end="")
+    print("*")
