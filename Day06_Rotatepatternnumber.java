707a1b60e2a8d07e3588a220c66724e7a90fbff9
vasistacv <vasisthamanju796@gmail.com>
2026-02-11 18:00:00 +0530

    Day06 Inverted Number Triangle

---
+++ Day06_Rotatepatternnumber.java
@@ -0,0 +1,24 @@
+
+/**
+ * Day 06: Inverted Number Triangle
+ * Printing numbers in an inverted pyramid shape (12345, 1234...).
+ */
+
+import java.util.*;
+
+public class Day06_Rotatepatternnumber {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.print("Enter the value of n:");
+        int n = sc.nextInt();
+
+        for (int i = 1; i <= n; i++) {
+            for (int j = 1; j <= n - i + 1; j++) {
+                System.out.print(j);
+            }
+            System.out.println();
+        }
+
+    }
+
+}
